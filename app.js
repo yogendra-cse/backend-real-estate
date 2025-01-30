830b3b6b93b9a217d36b282b711ea9b271509dcb
yogendrayogachitra <yogendra.yogachitra@gmail.com>
2025-01-30 17:01:53 +0530

    cors link two added

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import cookieParser from "cookie-parser";
 import mongoose from 'mongoose';
 import cors from "cors";
 const app = express();
-app.use(cors({ origin: "http://localhost:5173", credentials: true })); 
+app.use(cors({   origin: ["http://localhost:5173", "https://real-estate-frontend-project-kx4y.vercel.app"],  credentials: true })); 
 app.use(express.json());
 app.use(cookieParser());
 
